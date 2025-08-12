8d4486c16e008147bf7e7fce188ab6a822ee7802
CMI83 <isman.mihai@icloud.com>
2025-08-12 19:34:38 +0200

    Update MemberstackInit.tsx

--- components/MemberstackInit.tsx
+++ components/MemberstackInit.tsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 
 export default function MemberstackInit() {
   useEffect(() => {
-    // Nur im Browser laden (kein SSR, kein Prerender)
     (async () => {
       const memberstackDOM = (await import("@memberstack/dom")).default;
       memberstackDOM.init({ publicKey: "pk_89825d0662d17a373e2b" });
